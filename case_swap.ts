19ed9b6551b83bf177e7142e275c19f7556f0aff
christopher blodgett <christopher.blodgett@gmail.com>
2022-10-24 10:40:49 -0700

    init commit

---
+++ case_swap.ts
@@ -0,0 +1,12 @@
+function toAlternatingCase(s: string): string {
+  return s
+    .split("")
+    .map((l) => (l === l.toUpperCase() ? l.toLowerCase() : l.toUpperCase()))
+    .join("");
+}
+
+import { assert } from "chai";
+
+assert.equal(toAlternatingCase("hello world"), "HELLO WORLD");
+assert.equal(toAlternatingCase("HeLLo WoRLD"), "hEllO wOrld");
+assert.equal(toAlternatingCase("1a2b3c4d5e"), "1A2B3C4D5E");
---
+++ switch it up.ts
@@ -0,0 +1,22 @@
+const numKeys: { [key: number]: string } = {
+  0: "Zero",
+  1: "One",
+  2: "Two",
+  3: "Three",
+  4: "Four",
+  5: "Five",
+  6: "Six",
+  7: "Seven",
+  8: "Eight",
+  9: "Nine",
+}
+
+function switchItUp(intNumber: number): string {
+  return numKeys[intNumber];
+}
+
+import { assert } from "chai";
+
+assert.equal(switchItUp(1), "One");
+assert.equal(switchItUp(3), "Three");
+assert.equal(switchItUp(5), "Five");
